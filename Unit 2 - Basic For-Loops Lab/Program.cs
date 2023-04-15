de79a6f149d2b33664e4f8e3176f918f33b311b8
nik.kuehn <nik.kuehn@outlook.com>
2023-04-15 17:25:17 -0400

    Initial commit for Basic For Loop Lab

---
+++ Unit 2 - Basic For-Loops Lab/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Please enter a number.");
+
+int userinput = int.Parse(Console.ReadLine());
+
+int sum = 0;
+
+for (int i = 1; i <= userinput; i++)
+{
+    sum = sum + i;
+}
+
+Console.WriteLine(sum);
